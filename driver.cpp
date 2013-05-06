d66cc47f6255a385bf36e90e1955728900496ef6
Doug Jacobsen <dmjacobsen@lbl.gov>
2013-05-06 07:21:26 -0700

    repurpose driver for probing hdf5 files

--- driver.cpp
+++ driver.cpp
@@ -6,37 +6,34 @@
 #include <string.h>
 #include <strings.h>
 
+using namespace std;
+
 int main(int argc, char** argv) {
     int n = 300;
     try {
-        ProcAMQPIO output("genepool10.nersc.gov", 5672, "/", "guest", "guest", "procmon", 131072, FILE_MODE_WRITE);
-		output.set_context("localhost", "procmon", "driver");
+		ProcHDF5IO reader(argv[1], FILE_MODE_READ);
+		reader.set_context(argv[2], "whatever", "whatever");
 
-        procstat stat[n];
-        procdata data[n];
-        bzero(&stat, sizeof(procstat)*n);
-        bzero(&data, sizeof(procdata)*n);
-        for (int i = 0; i < n; i++) {
-            stat[i].pid = i;
-            data[i].pid = i;
-            if (i > 0) {
-                stat[i].ppid = i-1;
-                data[i].ppid = i-1;
-            } else {
-                stat[i].ppid = 0;
-                data[i].ppid = 0;
-            }
-            stat[i].realGid = i+5;
-            stat[i].effGid = i+1;
-            stat[i].state = 'R';
-            snprintf(data[i].execName, EXEBUFFER_SIZE, "MyExec%d", i);
-            snprintf(data[i].cmdArgs, BUFFER_SIZE, "MyExec%d", i);
-            data[i].cmdArgBytes = strlen(data[i].cmdArgs);
-            snprintf(data[i].exePath, BUFFER_SIZE, "/path/to/MyExec%d", i);
-            snprintf(data[i].cwdPath, BUFFER_SIZE, "/path/to/My/Home/%d", i);
-        }
-        output.write_procstat(stat, n);
-        output.write_procdata(data, n);
+		int nStat = reader.get_nprocstat();
+		int nData = reader.get_nprocdata();
+
+		cout << "procstat: " << nStat << endl;
+		cout << "procdata: " << nData << endl;
+
+		procdata all[nData];
+		reader.read_procdata(all, 0, nData);
+
+		for (int i = 0; i < nData; i++) {
+			cout << "[" << all[i].pid << "] " << all[i].execName << endl;
+			for (int j = 0; j < all[i].cmdArgBytes; j++ ){
+				if (all[i].cmdArgs[j] == 0) {
+					all[i].cmdArgs[j] = ' ';
+				}
+			}
+			all[i].cmdArgs[all[i].cmdArgBytes] = 0;
+			cout << "    " << all[i].cmdArgs << endl;
+		}
+
     } catch (ProcIOException& e) {
         std::cout << e.what() << std::endl;
     }
