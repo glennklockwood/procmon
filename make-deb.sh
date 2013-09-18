346a1aab1a0786de11551be784871d6ba9eb89f6
mattatlbl <mdunford@lbl.gov>
2013-09-18 15:27:18 -0700

    reworked script to use "debian" dir correctly

--- make-deb.sh
+++ make-deb.sh
@@ -3,6 +3,7 @@
 # make a debian package, run from genepool/procmon
 
 BUILDDIR=$BSCRATCH/procmon-build
+rm -rf $BUILDDIR
 [ -d "$BUILDDIR" ] || mkdir -p "$BUILDDIR"
 
 module purge
@@ -10,18 +11,13 @@ module load PrgEnv-gnu/4.8
 module load rabbitmq-c/0.4.0_nossl
 module load boost/1.54.0
 module load hdf5/1.8.11
-make -f Makefile clean
-make -f Makefile GENEPOOL=1 SECURED=1
+make clean
+make GENEPOOL=1 SECURED=1
 
 DIRS="usr etc"
 mkdir -p $BUILDDIR/usr/sbin
-mkdir -p $BUILDDIR/etc/cron.d
-cp runprocmon procmon ProcReducer $BUILDDIR/usr/sbin
-
-cat >$BUILDDIR/etc/cron.d/procmon <<EOF
-@reboot root test if /usr/sbin/runprocmon && /usr/sbin/runprocmon
-0 * * * * root test -f /usr/sbin/runprocmon && /usr/sbin/runprocmon
-EOF
+cp runprocmon procmon $BUILDDIR/usr/sbin
+cp -r debian $BUILDDIR/
 
 VER=$(awk '/^#define PROCMON_VERSION/{print $3}' procmon.hh | sed 's/"//g')
 
@@ -33,6 +29,9 @@ DESC="procmon - NERSC process monitor"
 fakeroot alien -g --version=$VER --description="$DESC" $TFILE
 
 DEST=procmon-$VER
+echo 8 >$DEST/debian/compat
+cp debian/* $DEST/debian/
+
 cat >$DEST/debian/control <<EOF
 Source: procmon
 Section: alien
